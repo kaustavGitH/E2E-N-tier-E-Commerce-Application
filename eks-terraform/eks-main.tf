bb04da564449b4a1d8a93a57c06e5598ae20f8d6
kaustavGitH <biswas.kaustav19@gmail.com>
2026-05-03 17:56:18 +0530

    updated eks authentication mode

--- eks-terraform/eks-main.tf
+++ eks-terraform/eks-main.tf
@@ -99,7 +99,7 @@ resource "aws_eks_cluster" "ecommerce_eks_cluster" {
   }
 
   access_config {
-    authentication_mode                         = "API"
+    authentication_mode                         = "API_AND_CONFIG_MAP"
     bootstrap_cluster_creator_admin_permissions = true
   }
 
